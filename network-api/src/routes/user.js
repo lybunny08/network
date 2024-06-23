f1d3e945ea6036e9f1f1585c5857f78966fd4229
Lydien Mamitiana <mamitiana22.aps2a@gmail.com>
2024-06-23 16:59:32 +0300

    con

--- network-api/src/routes/user.js
+++ network-api/src/routes/user.js
@@ -11,4 +11,10 @@ router.post('/login', userCtrl.login);
 router.put('/follow/:id', auth, userCtrl.follow);
 router.delete('/follow/:id', auth, userCtrl.unfollow);
 
+router.post('/connect/:id', auth, userCtrl.sendConnectionRequest);
+router.put('/connect/:connectReqId', auth, userCtrl.responseConnectionRequest);
+
+router.get('/network', auth, userCtrl.getNetworks);
+
+
 module.exports = router;
